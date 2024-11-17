366803eb8c99ab5a9c34f8e4e609b91431905df4
Vishal✨ <80511392+vishalonlyy@users.noreply.github.com>
2024-11-17 10:57:37 +0000

    Refactor Firebase initialization to conditionally set up Analytics in browser environment

--- estate/src/lib/firebase.ts
+++ estate/src/lib/firebase.ts
@@ -1,13 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { Analytics, getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAlS49Sc3FeTndunjx7xNnwrBoBBc0lxYk",
   authDomain: "teamshauryawebsite.firebaseapp.com",
@@ -21,4 +16,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+
+// Initialize Analytics only in browser environment
+let analytics: Analytics | undefined;
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+export { analytics };
